533cf62ba3284950269303aa8d33884dcaa5be3b
TanyaLark <tanyaenlang9898@gmail.com>
2024-02-24 19:22:49 +0200

    Refactor log and logError functions in file.js

--- HW_2/lib/logger/appenders/file.js
+++ HW_2/lib/logger/appenders/file.js
@@ -3,21 +3,28 @@ import * as constants from "../constants.js";
 import fs from "fs";
 import path from "path";
 
+const directory = `./log_output`;
+const fileName = `app.log`;
+const errorLogFileName = `app_error.js`;
+
 function log(date, level, category, message) {
-  const directory = `./log_output`;
-  const fileName = `app.log`;
   const filePath = path.join(directory, fileName);
-  const logMessage = helper.formatMessage(date, level, category, message);
+  const logMessage = `${helper.formatMessage(
+    date,
+    level,
+    category,
+    message
+  )}\n`;
 
   if (!fs.existsSync(directory)) {
     fs.mkdirSync(directory, { recursive: true });
   }
 
   if (level === constants.level.ERROR) {
-    logError(directory, JSON.stringify(logMessage));
+    logError(logMessage);
   }
 
-  fs.writeFileSync(filePath, `${logMessage}\n`, { flag: "a+" }, (err) => {
+  fs.writeFileSync(filePath, logMessage, { flag: "a+" }, (err) => {
     if (err) {
       console.error("Error writing log file:", err);
       return;
@@ -25,21 +32,15 @@ function log(date, level, category, message) {
   });
 }
 
-function logError(directory, logMessage) {
-  const errorLogFileName = `app_error.js`;
+function logError(logMessage) {
   const errorLogFilePath = path.join(directory, errorLogFileName);
 
-  fs.writeFileSync(
-    errorLogFilePath,
-    `${logMessage}\n`,
-    { flag: "a+" },
-    (err) => {
-      if (err) {
-        console.error("Error writing log file:", err);
-        return;
-      }
+  fs.writeFileSync(errorLogFilePath, logMessage, { flag: "a+" }, (err) => {
+    if (err) {
+      console.error("Error writing log file:", err);
+      return;
     }
-  );
+  });
 }
 
 export default { log };
