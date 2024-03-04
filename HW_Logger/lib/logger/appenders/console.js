742c417f5d77f9012b5a97482298e0e00196610c
TanyaLark <tanyaenlang9898@gmail.com>
2024-03-04 18:36:30 +0200

    Added stream API to console appender

--- HW_Logger/lib/logger/appenders/console.js
+++ HW_Logger/lib/logger/appenders/console.js
@@ -1,10 +1,24 @@
+import { Writable } from 'stream';
+
 function log(formatter) {
   return function (date, level, category, message) {
-    console.log(formatter(date, level, category, message));
+    const formattedMessage = formatter(date, level, category, message);
+    process.stdout.write(formattedMessage + '\n');
   };
 }
 
 function init(formatter) {
+  const logger = new Writable({
+    write(chunk, callback) {
+      process.stdout.write(chunk);
+      callback();
+    },
+  });
+
+  process.on('exit', () => {
+    logger.end();
+  });
+
   return {
     log: log(formatter),
   };
