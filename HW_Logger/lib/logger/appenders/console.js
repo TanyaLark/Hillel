3f86743d1a9c6413b015eeabe4a1666a7704dd1d
TanyaLark <tanyaenlang9898@gmail.com>
2024-03-04 20:10:13 +0200

    Added name of the file from which the process was launched to the  payload.

--- HW_Logger/lib/logger/appenders/console.js
+++ HW_Logger/lib/logger/appenders/console.js
@@ -1,26 +1,51 @@
 import { Writable } from 'stream';
+import transform from './helpers/fileHelper.js';
 
 function log(formatter) {
-  return function (date, level, category, message) {
-    const formattedMessage = formatter(date, level, category, message);
+  return function (date, level, category, message, filename) {
+    const formattedMessage = formatter(
+      date,
+      level,
+      category,
+      message,
+      filename
+    );
     process.stdout.write(formattedMessage + '\n');
   };
 }
 
 function init(formatter) {
   const logger = new Writable({
-    write(chunk, callback) {
+    write(chunk, encoding, callback) {
       process.stdout.write(chunk);
       callback();
     },
   });
+
+  const logWithFilename = (date, level, category, message) => {
+    const payload = transform.transformPayload(
+      date,
+      level,
+      category,
+      message,
+      transform.processFilename
+    );
+    log(formatter)(
+      payload.date,
+      payload.level,
+      payload.category,
+      payload.message,
+      payload.filename
+    );
+  };
 
   process.on('exit', () => {
     logger.end();
   });
 
   return {
-    log: log(formatter),
+    log: logWithFilename,
+    stream: logger,
   };
 }
 
--- HW_Logger/lib/logger/appenders/file.js
+++ HW_Logger/lib/logger/appenders/file.js
@@ -1,27 +1,42 @@
 import * as constants from '../constants.js';
 import fs from 'fs';
-import path from 'path';
 import config from '../config/config.js';
 import { formatMessage } from '../formatters/format-default-txt.js';
 import { Writable } from 'stream';
+import fileHelper from './helpers/fileHelper.js';
 
 const extension = config.formatter?.toLowerCase();
 const directory = constants.directory;
 const errorLogFileName = constants.errorLogFileName;
 
 function log(formatter) {
-  return function (date, level, category, message) {
-    const fileName = getFileName(date, extension);
-    const filePath = getFilePath(directory, fileName);
-    const errorLogFilePath = getFilePath(directory, errorLogFileName);
-    const logMessage = formatter(date, level, category, message);
+  return function (date, level, category, message, processFilename) {
+    const fileName = fileHelper.getFileName(date, extension);
+    const filePath = fileHelper.getFilePath(directory, fileName);
+    const errorLogFilePath = fileHelper.getFilePath(
+      directory,
+      errorLogFileName
+    );
+    const logMessage = formatter(
+      date,
+      level,
+      category,
+      message,
+      processFilename
+    );
 
     if (!fs.existsSync(constants.directory)) {
       fs.mkdirSync(constants.directory, { recursive: true });
     }
 
     if (level === constants.level.ERROR) {
-      const errLogMessage = formatMessage(date, level, category, message);
+      const errLogMessage = formatMessage(
+        date,
+        level,
+        category,
+        message,
+        processFilename
+      );
       writeToStream(errorLogFilePath, errLogMessage);
     }
     writeToStream(filePath, logMessage);
@@ -43,33 +58,25 @@ function writeToStream(filePath, message) {
   writableStream.end();
 }
 
-function getFormattedDateForCSVFileName(dateString) {
-  //example dateString = "2024-03-03T15:51:14.092Z";
-  const date = new Date(dateString);
-
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const year = date.getFullYear();
-
-  return `${month}_${day}_${year}`;
-}
-
-export function getFileName(date, formatter) {
-  const fileFormat = formatter.toUpperCase();
-
-  if (fileFormat === constants.formatters.CSV) {
-    return `app_${getFormattedDateForCSVFileName(date)}.csv`;
-  }
-  return `app.${fileFormat}`;
-}
-
-export function getFilePath(directory, fileName) {
-  return path.join(directory, fileName);
-}
-
 function init(formatter) {
+  const logWithFilename = (date, level, category, message) => {
+    const payload = fileHelper.transformPayload(
+      date,
+      level,
+      category,
+      message,
+      fileHelper.processFilename
+    );
+    log(formatter)(
+      payload.date,
+      payload.level,
+      payload.category,
+      payload.message,
+      payload.filename
+    );
+  };
   return {
-    log: log(formatter),
+    log: logWithFilename,
   };
 }
 
