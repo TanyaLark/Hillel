ae39136d786600148b339360c230d07374bf681d
TanyaLark <tanyaenlang9898@gmail.com>
2024-03-29 12:17:50 +0200

    Add sequelize for database connection and update dependencies

--- HW_Url_Shorter/app.js
+++ HW_Url_Shorter/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import webContext from './webContext.js';
 import dotenv from 'dotenv';
 import logger from 'logger';
+import sequelize from './config/db/sequelize.js';
 
 dotenv.config();
 const port = process.env.PORT || 3000;
@@ -9,6 +10,13 @@ const log = logger.getLogger('app.js');
 const app = express();
 
 webContext(app);
+
+try {
+  await sequelize.authenticate();
+  log.info('Postgres connection has been established successfully.');
+} catch (error) {
+  log.error('Unable to connect to the postgres database:', error);
+}
 
 app.listen(port, () => {
   log.info(`Server is running on port ${port}`);
--- HW_Url_Shorter/config/db/postgresql.js
+++ HW_Url_Shorter/config/db/postgresql.js
@@ -1,14 +1,14 @@
-import pg from 'pg';
-import dotenv from 'dotenv';
+// import pg from 'pg';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-export  const client = new pg.Client({
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  database: process.env.POSTGRES_DB,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-});
+// export  const client = new pg.Client({
+//   host: process.env.POSTGRES_HOST,
+//   port: process.env.POSTGRES_PORT,
+//   database: process.env.POSTGRES_DB,
+//   user: process.env.POSTGRES_USER,
+//   password: process.env.POSTGRES_PASSWORD,
+// });
 
-await client.connect();
+// await client.connect();
