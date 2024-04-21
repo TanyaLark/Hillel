2a82339c4f2931bab9d448f14cf8657ebae167b5
Tetiana Niehrieieva <tetiana.niehrieieva@itomy.ch>
2024-04-21 14:57:38 +0300

    Added helmet and cors middleware for improved security and cross-origin resource sharing

--- HW_Url_Shorter/app.js
+++ HW_Url_Shorter/app.js
@@ -5,13 +5,31 @@ import dotenv from 'dotenv';
 import logger from 'logger';
 import { Server } from 'socket.io';
 import { addWsConnection } from './common/wsConnections.js';
+import helmet from 'helmet';
+import cors from 'cors';
 
 dotenv.config();
 const port = process.env.PORT || 3000;
+const appAddress = process.env.APP_ADDRESS || `localhost:${port}`;
 const log = logger.getLogger('app.js');
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'", 'https://cdn.jsdelivr.net'],
+      },
+    },
+  })
+);
+const corsOptions = {
+  origin: `http://${appAddress}`,
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 webContext(app);
 
